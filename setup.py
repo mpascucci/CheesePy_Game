f8ae960428545b401f329a6197a66aa143b28912
Marco Pascucci <marpas.paris@gmail.com>
2024-03-12 18:34:15 +0100

    updated docs, preparing package gor PyPi

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='cheesepy_game',
-    version='1.1.2',
-    url='',
-    author='Marco Pascucci',
-    author_email='marpas.paris@gmail.com',
-    description='A pedagocig python game with mouse and cheese',
     install_requires=[
         "numpy>=1.22.4",
         "pygame>=2.5.2",
-        "labyrinth-py==1.0.4"
+        "labyrinth-py>=1.0.4"
     ],
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
