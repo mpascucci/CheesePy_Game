b3453ac93f8afed9e9c5b83275ee9459efe83191
Marco Pascucci <marpas.paris@gmail.com>
2024-03-19 21:07:19 +0100

    dependencies moved to project.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    install_requires=[
-        "numpy>=1.22.4",
-        "pygame>=2.5.2",
-        "labyrinth-py>=1.0.4"
-    ],
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     package_data={"": ["*.png", "*.ppm"]},
